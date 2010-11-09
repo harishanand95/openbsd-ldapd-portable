715b926c5777b4fa9f438fed2aeeade7eb7f6161
Martin Hedenfalk <martin@bzero.se>
2010-11-09 17:00:55 +0100

    Need to include config.h before testing for compatibility.

--- ldapd/ldapd.c
+++ ldapd/ldapd.c
@@ -16,6 +16,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include <sys/queue.h>
 #include <sys/types.h>
 #include <sys/wait.h>
